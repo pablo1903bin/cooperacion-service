ff9a93bc2aec7c1b8b5f66cb86bb6a9a86e14531
Pablo Rosas <50600873+pablo1903bin@users.noreply.github.com>
2025-08-12 15:05:59 -0600

    inicialice configuration

---
+++ src/main/java/com/tesoramobil/cooperacion/CooperacionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tesoramobil.cooperacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperacionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperacionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tesoramobil/cooperacion/CooperacionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesoramobil.cooperacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperacionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
